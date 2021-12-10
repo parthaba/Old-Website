25f6ff7d85527ea22c1397a678dda1cbc5f2bc55
Ameen Parthab <ameen.parthab@mail.utoronto.ca>
2021-12-10 10:36:47 -0500

    --- UI fixes and added section --- 1. Now works on mobile devices 2. fixed sizing so it fits screen 3. Added section for projects

---
+++ Components/dropdown/dropdown.js
@@ -0,0 +1,19 @@
+function showPython() {
+  document.getElementById("python").classList.toggle("show");
+  document.getElementById("python__button").classList.toggle("toggle");
+}
+
+function showJava() {
+  document.getElementById("java").classList.toggle("show");
+  document.getElementById("java__button").classList.toggle("toggle");
+}
+
+function showWebsites() {
+  document.getElementById("websites").classList.toggle("show");
+  document.getElementById("websites__button").classList.toggle("toggle");
+}
+
+function showDatabases() {
+  document.getElementById("databases").classList.toggle("show");
+  document.getElementById("databases__button").classList.toggle("toggle");
+}
